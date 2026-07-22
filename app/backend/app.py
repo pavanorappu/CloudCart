22f96712912bc24e8c81adefb58255f2dac3b4e2
pavanorappu <pavan08.work@gmail.com>
2026-07-22 05:45:23 +0000

    Add Prometheus metrics endpoint

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,7 +1,13 @@
 from flask import Flask, jsonify
 import mysql.connector
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+from flask import Response
 
 app = Flask(__name__)
+REQUEST_COUNT = Counter(
+    'cloudcart_requests_total',
+    'Total HTTP Requests'
+)
 
 def get_connection():
     return mysql.connector.connect(
@@ -13,6 +19,7 @@ def get_connection():
 
 @app.route("/")
 def home():
+    REQUEST_COUNT.inc()
     return jsonify({
         "message": "Welcome to CloudCart API - Version 3",
         "deployed_by":"Jenkins Kubernetes Pipeline"
@@ -20,12 +27,14 @@ def home():
 
 @app.route("/health")
 def health():
+    REQUEST_COUNT.inc()
     return jsonify({
         "status": "UP"
     })
 
 @app.route("/products")
 def products():
+    REQUEST_COUNT.inc()
     conn = get_connection()
     cursor = conn.cursor(dictionary=True)
 
@@ -36,6 +45,13 @@ def products():
     conn.close()
 
     return jsonify(result)
+
+@app.route("/metrics")
+def metrics():
+    return Response(
+        generate_latest(),
+        mimetype=CONTENT_TYPE_LATEST
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
